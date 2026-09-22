819b07f5ef82d73a673ec3ab08e9dfc13780bfbd
vladyslavbokovnia <2.76532526e+08+vladyslavbokovnia@users.noreply.github.com>
2026-09-22 22:36:29 +0000

    Replace repository with Android app and GitHub Actions build

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'clock-wallpaper-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './assets/clock-background.png'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
